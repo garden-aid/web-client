8be1d15a55f4703147d1191333847dbcb9a99f36
John McKim <johncmckim@gmail.com>
2016-10-03 15:42:51 +1000

    Rename auth form to see if that fixes module issue :(

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,6 @@
 
 exports.Layout = require('./layout/LayoutContainer');
-exports.Login = require('./auth/Login');
+exports.Login = require('./auth/LoginForm');
 exports.Callback = require('./auth/CallbackContainer');
 exports.NotFound = require('./NotFound');
 exports.UnderConstruction = require('./UnderConstruction');
